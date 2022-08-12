76965ab44528ec8b0e88917fd1497654b427f45b
Brian Foody <brianfoody@gmail.com>
2022-08-12 14:42:48 +0800

    Bump

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import DynamoWatcher from "./components/aws/DynamoWatcher";
 import BaseComponent from "./components/layout/BaseComponent";
 import * as Core from "./domain";
-export { Core, DynamoWatcher };
+export { Core, BaseComponent, DynamoWatcher };

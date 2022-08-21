747691746b02803e44f855c11e2e16469a5db727
Brian Foody <brianfoody@gmail.com>
2022-08-21 21:23:57 +0800

    Quick commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import BaseComponent from "./components/layout/BaseComponent/BaseComponent";
 import DynamoWatcher from "./components/aws/DynamoWatcher";
-import MainCanvas from "./components/spatial/MainCanvas";
+import MainCanvas from "./components/spatial/MainCanvas/MainCanvas";
 import SideMenu from "./components/layout/SideMenu/SideMenu";
 import AddResource from "./components/form/addResource";
 import * as Core from "./domain";
